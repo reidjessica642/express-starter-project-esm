728a64fa8dbb888795b67ea5ef4e9e6b622fc54a
reidjessica642 <reidjessica642@gmail.com>
2026-05-12 21:00:32 -0500

    Testing

--- mongo-test.js
+++ mongo-test.js
@@ -4,5 +4,25 @@ const client = new MongoClient(url);
 
 await client.connect();
 const db = client.db('arca');
-const result = await db.collection('widgets').findOne();
+const COLLECTION = 'widgets';
+const result = await db.collection('widgets').find({
+    color: 'green'
+}).toArray();
 console.log(result);
+
+// create/insert ----------------------------------
+/*
+const insertResult = await db.collection(COLLECTION).insertOne({
+    id: '6ba9f058-39c4-4b3f-b7f8-cabefbf8db83',
+    name: 'widget13',
+    color: 'green',
+    weight: 6,
+    inStock: true,
+    tags: ['tag1', 'tag2', 'tag3'],
+    createDate: new Date(),
+  });
+console.log(`Inserted ${insertResult.insertedCount} record with ID ${insertResult.insertedId}`);
+
+// const multiInsertResult = await db.collection(COLLECTION).insertMany([ { a: true}, { b: false } ]);
+// console.log(`Inserted ${multiInsertResult.insertedCount} records`);
+*/
